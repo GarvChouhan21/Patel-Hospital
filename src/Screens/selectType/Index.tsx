5c6bc495e918006f996445f8c0a6075959874255
apple <apple@apples-Mac-mini.local>
2024-07-19 18:10:43 +0530

    login  screen

---
+++ src/Screens/selectType/Index.tsx
@@ -0,0 +1,13 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+import { styles } from './style'
+
+const SelectType = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>SlectType</Text>
+    </SafeAreaView>
+  )
+}
+
+export default SelectType
